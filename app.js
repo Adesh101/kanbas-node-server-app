4aa699d9df5d05b7e90b7921f1d85cb1f2259062
Adesh Pingle <48215876+Adesh101@users.noreply.github.com>
2024-04-09 10:25:46 -0400

    db connection updates

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-import express from 'express';
-import Hello from './hello.js';
-import Lab5 from './lab5.js';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import CourseRoutes from './courses/routes.js';
-import ModuleRoutes from './modules/routes.js';
-import AssignmentRoutes from './assignments/routes.js';
-import UserRoutes from './users/routes.js';
-import session from 'express-session';
-import 'dotenv/config';
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import mongoose from "mongoose";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
 const CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 
-mongoose.connect(CONNECTION_STRING);
+//mongoose.connect(CONNECTION_STRING);
+mongoose
+  .connect(CONNECTION_STRING)
+  .then(() => {
+    console.log("Database connected");
+    app.listen(process.env.PORT || 4000);
+  })
+  .catch((err) => console.error("Error connecting to database", err));
+
 const app = express();
 app.use(
   cors({
@@ -23,15 +31,15 @@ app.use(
 );
 
 const sessionOptions = {
-  secret: 'any string',
+  secret: "any string",
   resave: false,
   saveUninitialized: false,
 };
 
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
-    sameSite: 'none',
+    sameSite: "none",
     secure: true,
   };
 }
